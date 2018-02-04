f6e12f381702491765a5fac0940e23569f7fa76e
Julián Unrrein <junrrein@gmail.com>
2018-02-04 18:20:30 -0300

    Terminar examen 16

--- Finales/Examen 16/examen16_sqlserver.sql
+++ Finales/Examen 16/examen16_sqlserver.sql
@@ -1,5 +1,63 @@
 -- Ejercicio 1.1
+SELECT s.Descrip
+FROM Sucursal s
+WHERE (SELECT sum(Cant)
+       FROM DepProd dp
+         INNER JOIN Deposito d ON dp.IDDeposito = d.IDDeposito
+       WHERE d.IDSucursal = s.IDSucursal)
+      >
+      (SELECT sum(Cant)
+       FROM LocalProd lp
+         INNER JOIN LocalVenta lv ON lp.IDLocal = lp.IDLocal
+       WHERE lv.IDSucursal = s.IDSucursal);
 
+-- Ejercicio 1.2
+BEGIN TRY
+BEGIN TRANSACTION;
+
+ALTER TABLE Local
+  DROP CONSTRAINT PK_Local;
+
+ALTER TABLE Local
+  ADD CONSTRAINT PK_Local
+PRIMARY KEY (IDLocal, Tipo);
+
+ALTER TABLE Local
+  ADD CONSTRAINT chk_Local_Tipo
+CHECK (Tipo IN ('D', 'V'));
+
+INSERT INTO Local
+  SELECT
+    lv.IDLocal,
+    lv.IDSucursal,
+    lv.Descrip,
+    'V'
+  FROM LocalVenta lv;
+
+INSERT INTO Local
+  SELECT
+    d.IDLocal,
+    d.IDSucursal,
+    d.Descrip,
+    'D'
+  FROM Deposito d;
+
+DECLARE
+@nTuplasLV INT = (SELECT count(*)
+                  FROM LocalVenta),
+@nTuplasD INT = (SELECT count(*)
+                 FROM Deposito),
+@nTuplasInsertadas INT = (SELECT count(*)
+                          FROM Local);
+
+IF @nTuplasLV + @nTuplasD <> @nTuplasInsertadas
+  RAISERROR ('', 12, 1);
+
+COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+ROLLBACK TRANSACTION;
+END CATCH
 
 -- Ejercicio 2
 ALTER PROCEDURE mostrarTablas
